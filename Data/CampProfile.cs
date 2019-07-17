c29d1f00ed8a6465337abc1d36927cce350c8546
Eray Izgin <erayizgin@gmail.com>
2019-07-17 22:29:06 +0300

    get talks

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -1,19 +1,28 @@
-﻿using AutoMapper;
-using CoreCodeCamp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Models;
 
 namespace CoreCodeCamp.Data
 {
-    public class CampProfile : Profile
+  public class CampProfile : Profile
+  {
+    public CampProfile()
     {
-        public CampProfile()
-        {
-            this.CreateMap<Camp, CampModel>()
-                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
-                .ReverseMap();
-        }
+      this.CreateMap<Camp, CampModel>()
+        .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+        .ReverseMap();
+
+      this.CreateMap<Talk, TalkModel>()
+        .ReverseMap()
+        .ForMember(t => t.Camp, opt => opt.Ignore())
+        .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+      this.CreateMap<Speaker, SpeakerModel>()
+        .ReverseMap();
     }
+  }
 }
